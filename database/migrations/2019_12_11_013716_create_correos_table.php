da10d4a21f93e096fcd89524851e84dec979c8bd
Leandro Moreno <123857@gmail.com>
2019-12-10 21:13:03 -0500

    Migrations-instalacion

---
+++ database/migrations/2019_12_11_013716_create_correos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCorreosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('correos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamp('fecha_creacion')->nullable();
+            $table->longText('mensaje');
+            $table->string('imagen');
+            $table->biginteger('usuario_id')->unsigned();
+            $table->foreign('usuario_id')->references('id')->on('users');
+            $table->integer('estado')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('correos');
+    }
+}
