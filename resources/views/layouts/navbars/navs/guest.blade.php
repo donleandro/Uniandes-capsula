66f4bce9d873e00bc143c58b3dfb8cb8f14253ac
Leandro Moreno <123857@gmail.com>
2019-12-12 16:16:59 -0500

    react + sass

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -1,5 +1,5 @@
 <!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top text-white">
+<nav class="navbar navbar-expand-lg bg-primary navbar-absolute fixed-top text-white">
   <div class="container">
     <div class="navbar-wrapper">
       <a class="navbar-brand" href="{{ route('home') }}">{{ $title }}</a>
