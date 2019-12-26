702d130a1f54b2dbae9f7125845efefcdd7ff07b
Leandro Moreno <123857@gmail.com>
2019-12-26 14:49:37 -0500

    Se cambia la visualización de la imagen, ahora tiene el estado de foto tomada y cierra la camara

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,123 +5,72 @@
 
 require('./bootstrap');
 
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+
 import Camera, {
     FACING_MODES,
     IMAGE_TYPES
 } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
+
+import ImagePreview from './ImagePreview';
+
 import axios from 'axios';
 
 function App(props) {
+    const [dataUri, setDataUri] = useState('');
+    const isFullscreen = false;
 
     function fileUpload(image) {
-        // const url = 'https://capsula.test/insert';
-        // console.log('takePhoto');
-        // const formData = {
-        //     file: image
-        // }
-        // return post(url, formData)
-        //     .then(response => console.log(response))
-
         axios.post('/insert', {
-            imagen: image
-        })
-        .then(function (response) {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error.response);
-        });
-
+                imagen: image
+            })
+            .then(function(response) {
+                console.log(response.data);
+            })
+            .catch(function(error) {
+                console.log(error.response);
+            });
     }
 
 
-function handleTakePhoto(dataUri) {
-    // Do stuff with the photo...
-    console.log('takePhoto');
-    fileUpload(dataUri);
-}
+    function handleTakePhoto(dataUri) {
+        // Do stuff with the photo...
+        console.log('takePhoto');
+        fileUpload(dataUri);
+    }
 
-function handleTakePhotoAnimationDone(dataUri) {
-    // Do stuff with the photo...
-    console.log('takePhoto');
-}
-
-function handleCameraError(error) {
-    console.log('handleCameraError', error);
-}
-
-function handleCameraStart(stream) {
-    console.log('handleCameraStart');
-}
-
-function handleCameraStop() {
-    console.log('handleCameraStop');
-}
-
-return ( <
-    Camera onTakePhoto = {
-        (dataUri) => {
-            handleTakePhoto(dataUri);
-        }
+    function handleTakePhotoAnimationDone(dataUri) {
+        console.log('takePhoto');
+        setDataUri(dataUri);
     }
-    onTakePhotoAnimationDone = {
-        (dataUri) => {
-            handleTakePhotoAnimationDone(dataUri);
-        }
-    }
-    onCameraError = {
-        (error) => {
-            handleCameraError(error);
-        }
-    }
-    idealFacingMode = {
-        FACING_MODES.ENVIRONMENT
-    }
-    idealResolution = {
-        {
-            width: 640,
-            height: 480
-        }
-    }
-    imageType = {
-        IMAGE_TYPES.JPG
-    }
-    imageCompression = {
-        0.97
-    }
-    isMaxResolution = {
-        true
-    }
-    isImageMirror = {
-        false
-    }
-    isSilentMode = {
-        false
-    }
-    isDisplayStartCameraError = {
-        true
-    }
-    isFullscreen = {
-        false
-    }
-    sizeFactor = {
-        1
-    }
-    onCameraStart = {
-        (stream) => {
-            handleCameraStart(stream);
-        }
-    }
-    onCameraStop = {
-        () => {
-            handleCameraStop();
-        }
-    }
-    />
-);
+    return ( <
+        div > {
+            (dataUri) ?
+            < ImagePreview dataUri = {
+                dataUri
+            }
+            isFullscreen = {
+                isFullscreen
+            }
+            /> :
+                < Camera onTakePhotoAnimationDone = {
+                handleTakePhotoAnimationDone
+            }
+            isFullscreen = {
+                isFullscreen
+            }
+            imageType = {
+                IMAGE_TYPES.JPG
+            }
+            isMaxResolution = {
+                true
+            }
+            />
+        } <
+        /div>
+    );
 }
 
 export default App;
