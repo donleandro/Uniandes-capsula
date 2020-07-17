730c1ab8d4badf42631c35fc9eb42489f233b00f
Leandro Moreno <123857@gmail.com>
2020-07-17 11:35:51 -0500

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -101,6 +101,21 @@ class LoginController extends Controller
     {
          return view('auth.login-local');
     }
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
 
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return $this->loggedOut($request) ?: redirect('/login');
+    }
 
 }
