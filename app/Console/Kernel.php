f525b447be7728d8b65ad8cf533f05a71e033f68
Leandro Moreno <123857@gmail.com>
2020-07-30 17:09:27 -0500

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -65,10 +65,12 @@ class Kernel extends ConsoleKernel
           $fecha  = new Carbon($pod->created_at);
           return $fecha->diffInDays($hoy)<1;
         });
-        dd(Storage::disk('dropbox')->search('https://www.dropbox.com/s/xn6ms6y9li82kl1/lea_1579029427.png?dl=0'));
+        // download('home/Aplicaciones/CapsulaDelTiempoUniandes/lea_1579029427.png'
+        // dd(Storage::disk('dropbox')->get('https://www.dropbox.com/s/xn6ms6y9li82kl1/lea_1579029427.png?dl=0'));
+        dd(Storage::disk('dropbox')->get('xn6ms6y9li82kl1'));
         foreach ($podsHoy as $pod) {
           $usuario = User::where('id', $pod->usuario_id)->first();
-          // Notification::send($usuario, new EnviarPod( $pod ));
+          Notification::send($usuario, new EnviarPod( $pod ));
         }
         // $date = Carbon::now();
         // print_r($date);
