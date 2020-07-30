b97ce624a9a6d085e64bb946b3029b90e653f675
Leandro Moreno <123857@gmail.com>
2020-07-30 16:57:06 -0500

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Collection;
 use Illuminate\Http\File;
 use Spatie\Dropbox\Client;
 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+
 
 class Kernel extends ConsoleKernel
 {
@@ -65,7 +68,7 @@ class Kernel extends ConsoleKernel
         dd(Storage::disk('dropbox')->get('https://www.dropbox.com/s/xn6ms6y9li82kl1/lea_1579029427.png?dl=0'));
         foreach ($podsHoy as $pod) {
           $usuario = User::where('id', $pod->usuario_id)->first();
-          Notification::send($usuario, new EnviarPod( $pod ));
+          // Notification::send($usuario, new EnviarPod( $pod ));
         }
         // $date = Carbon::now();
         // print_r($date);
