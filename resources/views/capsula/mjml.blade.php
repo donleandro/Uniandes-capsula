4b2ccb322664bc6c3a335820edb6bead1bd05055
Leandro Moreno <123857@gmail.com>
2020-08-01 20:28:42 -0500

    Update mjml.blade.php

--- resources/views/capsula/mjml.blade.php
+++ resources/views/capsula/mjml.blade.php
@@ -2,14 +2,14 @@
   <mj-head>
     <mj-attributes>
       <mj-all padding="0px"></mj-all>
-      <mj-class name="preheader" color="#000000" font-size="11px" font-family="Ubuntu, Helvetica, Arial, sans-serif" padding="0 20px"></mj-class>
+      <mj-class name="preheader" color="#cccccc" font-size="11px" font-family="Ubuntu, Helvetica, Arial, sans-serif" padding="0 20px"></mj-class>
       <mj-class name="cta" color="#FFFFFF" font-size="13px" border-radius="3px" href="https://mjml.io" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" padding="20px 25px"></mj-class>
       <mj-class name="footer-text" align="center" color="#ffffff" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="15px" padding="0 25px"></mj-class>
       <mj-navbar-link padding="0 35px" font-weight="bold" font-size="12px"></mj-navbar-link>
     </mj-attributes>
     <mj-style inline="inline">a { text-decoration: none; color: inherit; }</mj-style>
   </mj-head>
-  <mj-body background-color="#d2eeff">
+  <mj-body background-color="#000000">
     <mj-section padding-bottom="20px" padding-top="20px">
       <mj-group>
         <mj-column>
@@ -20,19 +20,9 @@
         </mj-column>
       </mj-group>
     </mj-section>
-    <mj-section background-color="#ffffff">
+    <mj-section background-color="#000000">
       <mj-column>
-        <mj-image src="http://191n.mj.am/img/191n/3s/x0j.png" alt="Worldly logo" align="center" width="105px" padding="10px 0"></mj-image>
-      </mj-column>
-    </mj-section>
-    <mj-section background-color="#ffe118" padding="20px">
-      <mj-column>
-        <mj-navbar base-url="https://mjml.io" hamburger="hamburger" ico-color="#444">
-          <mj-navbar-link href="https://mjml.io">home</mj-navbar-link>
-          <mj-navbar-link href="https://mjml.io">Summer deals</mj-navbar-link>
-          <mj-navbar-link href="https://mjml.io">Our blog</mj-navbar-link>
-          <mj-navbar-link href="https://mjml.io">Follow us</mj-navbar-link>
-        </mj-navbar>
+        <mj-image src="{{ asset('img') }}/logoUniandes.svg" alt="Logo Uniandes" align="center" width="105px" padding="10px 0"></mj-image>
       </mj-column>
     </mj-section>
     <mj-section background-url="http://191n.mj.am/img/191n/3s/xm9.jpg" background-repeat="no-repeat" background-size="cover" padding="20px 0">
@@ -43,60 +33,14 @@
         <mj-text align="center" color="#ffffff" line-height="32px" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="28px" padding-top="180px">OR <br /> CITYSCAPE?</mj-text>
       </mj-column>
     </mj-section>
-    <mj-section background-color="#ffffff" padding="20px 0">
+    <mj-section background-color="#000000" padding="20px 0">
       <mj-column>
-        <mj-text align="center" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="15px" padding="10px 25px" color="#BD8714">
-          <p>SUNNIEST DESTINATIONS</p>
-        </mj-text>
-        <mj-image src="http://191n.mj.am/img/191n/3s/x2w.jpg" alt="Maldives, Corse : -15%" width="240px"></mj-image>
-        <mj-button mj-class="cta" background-color="#bd8714">BOOK NOW</mj-button>
-        <mj-image src="http://191n.mj.am/img/191n/3s/x2x.jpg" alt="Tahiti, Bora Bora : -10 %" width="240px" padding="10px 25px"></mj-image>
-        <mj-button mj-class="cta" background-color="#bd8714">BOOK NOW</mj-button>
+        <mj-image src="{{asset('storage/' . $image)}}" alt="Maldives, Corse : -15%" width="240px"></mj-image>
+        <mj-button mj-class="cta" background-color="#bd8714">Verlo en el explorador</mj-button>
       </mj-column>
       <mj-column>
-        <mj-text align="center" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="15px" padding="10px 25px" color="#BD8714">
-          <p>CHEAP CITY TOUR</p>
-        </mj-text>
-        <mj-image src="http://191n.mj.am/img/191n/3s/x20.jpg" alt="Stockholm, Rome : from 162 € / day" width="240px"></mj-image>
-        <mj-button mj-class="cta" background-color="#48b6bf">BOOK NOW</mj-button>
-        <mj-image src="http://191n.mj.am/img/191n/3s/x2s.jpg" alt="New York, London : -30 %" width="240px" padding="10px 25px"></mj-image>
-        <mj-button mj-class="cta" background-color="#48b6bf">BOOK NOW</mj-button>
-      </mj-column>
-    </mj-section>
-    <mj-section background-color="#ffffff" padding="20px 0">
-      <mj-column>
-        <mj-image href="https://mjml.io" src="http://191n.mj.am/img/191n/3s/x21.jpg" alt="NEW : Donwload your city guide" width="250px" padding="0 25px"></mj-image>
-      </mj-column>
-      <mj-column>
-        <mj-text align="center" color="#000000" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="13px" padding="0 25px" font-weight="bold">
-          <p>RATE YOUR STAY !</p>
-        </mj-text>
-        <mj-image href="https://mjml.io" src="http://191n.mj.am/img/191n/3s/x2l.png" alt="Book rate" width="131px"></mj-image>
-      </mj-column>
-    </mj-section>
-    <mj-section background-color="#4b525c">
-      <mj-column width="33%" vertical-align="top">
-        <mj-image src="http://191n.mj.am/img/191n/3s/x09.png" alt="Best companies for your flights" width="30px" padding="10px 25px"></mj-image>
-        <mj-text mj-class="footer-text">
-          <p>
-            BEST COMPANIES FOR YOUR FLIGHTS
-          </p>
-        </mj-text>
-      </mj-column>
-      <mj-column width="33%">
-        <mj-image src="http://191n.mj.am/img/191n/3s/xsw.png" alt="Insurances included" width="29px" padding="10px 25px"></mj-image>
-        <mj-text mj-class="footer-text">
-          <p>
-            INSURANCES INCLUDED
-          </p>
-        </mj-text>
-      </mj-column>
-      <mj-column width="33%" vertical-align="top">
-        <mj-image src="http://191n.mj.am/img/191n/3s/xsx.png" alt="Hotels best selection" width="31px" padding="10px 25px"></mj-image>
-        <mj-text mj-class="footer-text">
-          <p>
-            HOTELS BEST SELECTION
-          </p>
+        <mj-text align="center" color="#ffee00" font-family="Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif" font-size="13px" padding="0 25px" font-weight="bold">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod vel sapien non ultricies. Duis id est et metus cursus volutpat eget a leo. Donec id interdum eros. Suspendisse lobortis nibh at purus aliquam vestibulum. Mauris lacus dui, volutpat ut aliquam sit amet, facilisis eget tellus. Morbi eleifend libero nibh, vel dapibus nunc varius vel. Aenean molestie est sed odio posuere congue. Mauris interdum luctus elit, sed interdum nisl maximus a. Etiam efficitur elit velit, vel eleifend risus suscipit in. Nullam malesuada dapibus risus et viverra.</p>
         </mj-text>
       </mj-column>
     </mj-section>
