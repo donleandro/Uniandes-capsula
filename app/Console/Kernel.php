c09c3c81f16b05285adad11199b3876f71aa36cb
Leandro Moreno <123857@gmail.com>
2020-08-04 12:15:25 -0500

    Prod version

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -49,8 +49,8 @@ class Kernel extends ConsoleKernel
       //Buscar pod con correo de hace 1 año. Comparar con la fecha actual...
       //enviar esos correos
       //editar vista del capsulecorp
-      $hoy = Carbon::createFromFormat('Y/m/d H:i:s',  '2021/01/14 19:17:11');
-        // $hoy = Carbon::now();
+      // $hoy = Carbon::createFromFormat('Y/m/d H:i:s',  '2021/01/14 19:17:11');
+        $hoy = Carbon::now();
         $hoy->subDays(365);
         $pods = Correo::whereYear('created_at',$hoy->year)
                         ->whereMonth('created_at',$hoy->month)
